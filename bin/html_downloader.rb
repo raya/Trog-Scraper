1890cd0b9048eae5dff98bb4418ddb4903a097c8
RAYA DESAWADE <rayalynn@gmail.com>
2012-10-07 11:24:28 +0700

    Refactored LinkExtractor class, added example/test sites to html_downloader

--- bin/html_downloader.rb
+++ bin/html_downloader.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'ruby-readability'
 require 'securerandom'
 require 'mechanize'
+
 load '../lib/link_extractor.rb'
 
 #Future
@@ -56,9 +57,8 @@ def website_encoding_tag
 end
 
 def write_file(page_url)
-  #output_file = random_filename 
-  output_file = "test.html"
-  File.open(output_file, "a") do |file|
+  output_file = random_filename 
+  File.open(output_file, "w") do |file|
     source_page = open(page_url).read
     file.write website_encoding_tag
     doc = Readability::Document.new(source_page)
@@ -74,18 +74,17 @@ def write_file(page_url)
     file.write "<hr>"
   end
 end
-
-
 #crawl website
 #agent = Mechanize.new()
 #regex = ".subj-link"
 #puts agent.get(page_url).search(regex)#:w.map(&:text).map(&:strip)
 
-#gets the URLs of main posts from a livejournal website
-def get_livejournal_posts(starting_page=0, max_pages=1) 
+#get all links which lead to individual posts
+def get_link_list
   regex = "a.subj-link"
   link_list = []
-  next_page_script = "?skip="
+  starting_page = 0
+  max_pages = 1 
 
   starting_page.upto(max_pages) do |count|
     current_page = base_url + "/" + next_page_script + (count * 10).to_s
@@ -94,77 +93,38 @@ def get_livejournal_posts(starting_page=0, max_pages=1)
       link_list << link['href']
     end
   end
+  puts link_list
 end
-
-#open each page in an array of links and write them to a file
-def write_links_to_file(link_list)
+#open each page and write it to a file
+def write_to_file
   link_list.each do |link|
     write_file(link)  
   end
 end
 
 
-#Gets the URLs of posts from a SETT site
-def get_sett_posts(max_entries=5, page_url)
-  matcher = "div.content a.title.fastload"
-  link_list = []
-  page = Nokogiri::HTML(open(page_url))
-  links = page.css(matcher).each do |link|
-    link_list << link['href'] 
-    write_link
-  end
-  return link_list
-end
-
-#gets posts on an index page based on the post and next_page matchers
-def get_posts_from_index(page_url, max_entries=5, post_matcher, next_page_matcher)
-  link_list = []
-  post_count = 0
-
-  begin
-    #open page
-  end while link_list.length < max_entries
-
-  page = Nokogiri::HTML(open(page_url))
-  page.css(post_matcher).each do |link|
-    link_list << link['href']
-    post_count += 1
-    break if post_count >= max_entries
-  end
-  puts link_list
-end
-
-def get_posts_from_page(page_url, max_entries, post_matcher, link_list)
-  page = Nokogiri::HTML(open(page_url))
-  page.css(post_matcher).each do |link|
-    link_list << link['href']
-    post_count += 1
-    break if post_count >= max_entries
-  end
-  return link_list 
-end
-
-#Sett Blog
+#SETT Bloc
 #page_url = "http://sebastianmarshall.com"
-#post_matcher = "div.content a.title.fastload"
-#next_page_string = "page/"
-#site = LinkExtractor.new(page_url, 20, post_matcher, next_page_string,0, 1)
-#site.get_posts_via_url
-#puts site.link_list
+#max_entries = 20
+#next_page_matcher = "page/"
+#starting_page = 1
+#starting_page_incrementor = 1
+#post_matcher = ""
 
-#livejournal squid314
-#page_url = "http://squid314.livejournal.com"
-#post_matcher = "td.caption a.subj-link"
-#next_page_string = "?skip="
-#starting_page = 0
-#site = LinkExtractor.new(page_url, 20, post_matcher, next_page_string,0,10)
-#site.get_posts
-#puts site.link_list
+page_url = "http://squid314.livejournal.com"
+max_entries = 20
+next_page_matcher = "?skip="
+post_matcher = "td.caption a.subj-link"
+starting_page = 0
+starting_page_incrementor = 10
 
-#maria blog
-page_url = "http://just-eat-your-cupcake.blogspot.com"
-post_matcher = "h3.post-title.entry-title a"
-next_page_string = "a#Blog1_blog-pager-older-link"
-site = LinkExtractor.new(page_url, 20, post_matcher, next_page_string,0,-1)
+#page_url = "http://just-eat-your-cupcake.blogspot.com/"
+#max_entries = 20
+#next_page_matcher = "a.blog-pager-older-link"
+#post_matcher = "h3.post-title.entry-title a"
+#starting_page = -1
+#starting_page_incrementor = -1
+
+site = LinkExtractor.new(page_url, max_entries, post_matcher, next_page_matcher,
+                         starting_page, starting_page_incrementor)
 site.get_posts
-puts site.link_list
--- lib/link_extractor.rb
+++ lib/link_extractor.rb
@@ -2,6 +2,8 @@ require 'nokogiri'
 require 'open-uri'
 
 class LinkExtractor 
+  # Scrapes a website and stores the links to its posts in an array 
+
   attr_reader :link_list
 
   def initialize(page_url, max_entries=10, post_matcher, next_page_matcher,
@@ -15,36 +17,29 @@ class LinkExtractor
     @link_list = []
   end
 
-  #Get list of posts based on a script, ex. blog.com/page/3
-  def get_posts_via_url
-
-    while post_limit_not_hit? 
-      current_url = @page_url + "/" + @next_page_matcher + @starting_page.to_s
-      puts "Opening: " + current_url
-      process_page(current_url)
-      @starting_page += @starting_page_incrementor 
-    end
-  end
-
-  #get list of posts on page based on a css attribute
-  def get_posts_via_css
-    current_page = @page_url 
-    while post_limit_not_hit?
-      puts "Opening: " + current_page
-      process_page(current_page)
-      next_page = Nokogiri::HTML(open(current_page))
-      current_page = next_page.css(@next_page_matcher)[0]['href']
-    end
-  end
-
-  #get posts. If starting_page_incrementor is -1, the next page will
-  #be determined by css
-  def get_posts
-    if @starting_page_incrementor == -1
-      get_posts_via_css
+  def get_next_page_url(current_page=@page_url)
+    if search_via_css? 
+      "Puts Searching CSS"
+      page = Nokogiri::HTML(open(current_page))
+      next_page = page.css(@next_page_matcher)[0]['href']
     else
-      get_posts_via_url
+      puts "Searching URL"
+      @starting_page += @starting_page_incrementor
+      next_page = @page_url + "/" + @next_page_matcher + @starting_page.to_s
     end
+  end
+
+  def get_posts
+    current_url = @page_url
+    while post_limit_not_hit?
+      puts "Opening: " + current_url 
+      process_page(current_url)
+      current_url = get_next_page_url(current_url)
+    end
+  end
+
+  def search_via_css?
+    @starting_page_incrementor == -1
   end
 
   #Put links to main posts in array link_list
@@ -54,6 +49,8 @@ class LinkExtractor
       link_list << link['href']
       break if link_list.length >= @max_entries
     end
+    puts "Current link_list: "
+    puts link_list
   end
 
   def post_limit_not_hit?
