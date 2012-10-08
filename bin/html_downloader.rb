47810c3aa43e3da7eb8c38df3e0265ebb380d24d
RAYA DESAWADE <rayalynn@gmail.com>
2012-10-08 12:48:14 +0700

    Added table of contents and links to individual posts for use in kindle documents. Changed break tag so it doesn't error out in kindlegen.

--- bin/html_downloader.rb
+++ bin/html_downloader.rb
@@ -28,34 +28,42 @@ def write_html_ending(filename)
   end
 end
 
-def write_file(page_url, output_file)
+def write_file(page_url, output_file, index)
   File.open(output_file, "a") do |file|
     source_page = open(page_url).read
     doc = ContentExtractor.new(source_page)
-    file.write "Page URL: "
-    file.write page_url
-    file.write "</br>" 
-    file.write "Title: "
-    file.write doc.title_info
-    file.write "</br>" 
-    file.write "Author: "
-    file.write doc.author_info
-    file.write "</br>"
+    file.write "<mbp:pagebreak />"
+    file.write "<h3><a name=\"post-#{index+1}\">#{doc.title_info}</a></h3>"
+    file.write "<h5>Page URL: "
+    file.write page_url + "</h5>"
+    file.write "<h5>Author: "
+    file.write doc.author_info + "</h5>"
     file.write doc.content
-    file.write "<hr>"
+
   end
 end
 
 def create_html_file(link_list)
   output_file = random_filename
   write_html_headers(output_file)
-
-  link_list.each do |link|
+  create_toc(output_file, link_list.length)
+  link_list.each_with_index do |link, index|
     puts "Processing #{link}"
-    write_file(link, output_file)
+    write_file(link, output_file, index)
   end
 
   write_html_ending(output_file)
+end
+
+def create_toc(filename, index)
+  File.open(filename, "a") do |file|
+    file.write "<h3>Table of Contents</h3>"
+    file.write "<br>"
+    1.upto(index) do |i|
+      file.write "<a href=\"#post-#{i}\">Post #{i}</a>"
+      file.write "<br><br>"
+    end
+  end
 end
 
 #options = { page_url: 'http://tynan.com',
@@ -67,19 +75,19 @@ end
 
 #squid314 LJ
 #options = { page_url: 'http://squid314.livejournal.com',
-  #max_entries: 20,
-  #next_page_matcher: "?skip=",
-  #post_matcher: "td.caption a.subj-link",
-  #starting_page: 0,
-  #starting_page_incrementor: 10 }
+#max_entries: 20,
+#next_page_matcher: "?skip=",
+#post_matcher: "td.caption a.subj-link",
+#starting_page: 0,
+#starting_page_incrementor: 10 }
 
 #JEYC
 options = { page_url: 'http://just-eat-your-cupcake.blogspot.com',
-max_entries: 20,
-next_page_matcher: "a.blog-pager-older-link",
-post_matcher: "h3.post-title.entry-title a",
-starting_page: -1,
-starting_page_incrementor: -1 }
+  max_entries: 5,
+  next_page_matcher: "a.blog-pager-older-link",
+  post_matcher: "h3.post-title.entry-title a",
+  starting_page: -1,
+  starting_page_incrementor: -1 }
 
 site = LinkExtractor.new(options)
 site.get_posts
--- bin/test.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-require 'readability'
-require 'open-uri'
-
-source = open('http://squid314.livejournal.com/332946.html').read
-puts Readability::Document.new(source).content
